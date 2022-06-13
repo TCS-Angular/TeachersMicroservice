4815f17977383531b94c9cbad36df3973125cd34
ngactive99 <ngactive99@gmail.com>
2022-06-13 15:33:05 +0530

    added controller

--- src/main/java/com/teacher/controller/TeacherController.java
+++ src/main/java/com/teacher/controller/TeacherController.java
@@ -1,5 +1,27 @@
 package com.teacher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teacher.entity.Teacher;
+import com.teacher.service.TeacherService;
+
+@RestController
+@RequestMapping("/teacher")
 public class TeacherController {
+	
+	@Autowired
+	private TeacherService teacherService;
+	
+	
+	@GetMapping("/{cId}")
+	public Teacher getTeacher(@PathVariable("cId") Long cId) {
+		return this.teacherService.getTeacher(cId);
+		
+	}
+	
 
 }
