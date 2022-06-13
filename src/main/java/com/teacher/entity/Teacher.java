4ff38c703b7c80c20725e977da22f74f5e04afff
ngactive99 <ngactive99@gmail.com>
2022-06-13 14:58:29 +0530

    added both entity

--- src/main/java/com/teacher/entity/Teacher.java
+++ src/main/java/com/teacher/entity/Teacher.java
@@ -1,5 +1,68 @@
 package com.teacher.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher {
+	
+	private Long cId;
+	private String name;
+	private String subject;
+	
+	List<Student> students = new ArrayList<>();
+
+	public Teacher(Long cId, String name, String subject, List<Student> students) {
+		super();
+		this.cId = cId;
+		this.name = name;
+		this.subject = subject;
+		this.students = students;
+	}
+
+	public Teacher(Long cId, String name, String subject) {
+		super();
+		this.cId = cId;
+		this.name = name;
+		this.subject = subject;
+	}
+
+	public Teacher() {
+		super();
+	}
+
+	public Long getcId() {
+		return cId;
+	}
+
+	public void setcId(Long cId) {
+		this.cId = cId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	
+	
 
 }
